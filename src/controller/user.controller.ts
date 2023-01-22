f3ca02bfe74c2d9c050fa2bbbef7ede0e3fb99da
alexis mouchon <alexismouchon09@gmail.com>
2023-01-22 22:23:52 +0100

    ✨ add controller and service for user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,15 +1,72 @@
 import { Request, Response } from "express";
 import { omit } from "lodash";
-import { createUser } from "../service/user.service";
+import {
+  createUser,
+  getUsers,
+  loginUser,
+  verifyToken,
+  _getUser,
+} from "../service/user.service";
 import Log from "../utils/Log";
 
 export const createUserHandler = async (req: Request, res: Response) => {
-  req;
-  res;
   Log.info("/GET /api/users");
   try {
     const user = await createUser(req.body);
     return res.status(201).send(omit(user?.toJSON(), "password"));
+  } catch (error: any) {
+    Log.error(error.message);
+    return res.status(409).send(error.message);
+  }
+};
+
+export const loginUserHandler = async (req: Request, res: Response) => {
+  Log.info("/GET /api/users/login");
+  try {
+    const user = await loginUser(req.body);
+    return res.status(200).send(user);
+  } catch (error: any) {
+    Log.error(error.message);
+    return res.status(409).send(error.message);
+  }
+};
+
+export const getAllUsersHandler = async (_: Request, res: Response) => {
+  Log.info("/GET /api/users");
+  try {
+    const users = await getUsers();
+    if (!users) {
+      return res.status(404).send("No users found");
+    }
+    return res.status(200).send(users);
+  } catch (error: any) {
+    Log.error(error.message);
+    return res.status(409).send(error.message);
+  }
+};
+
+export const getCurrentUserHandler = async (req: Request, res: Response) => {
+  Log.info("/GET /api/checkLog");
+
+  const token = req.headers.authorization?.split("Scla ")[1];
+  if (!token) {
+    return res.status(401).send("No token found");
+  }
+
+  try {
+    const user = verifyToken(token);
+    if (!user) {
+      return res.status(404).send("No user found");
+    }
+    if (typeof user === "object" && user) {
+      const userInfo = await _getUser(user.id);
+      return res.status(200).json({
+        jwtInfo: user,
+        userInfo: userInfo,
+      });
+    } else {
+      return res.status(404).send("No user found");
+    }
   } catch (error: any) {
     Log.error(error.message);
     return res.status(409).send(error.message);
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,8 +1,45 @@
 import { DocumentDefinition } from "mongoose";
 import User, { UserDocument } from "../model/user.model";
+import jwt from "jsonwebtoken";
 import Log from "../utils/Log";
 
 const SECRET_KEY = process.env.SECRET_KEY || "secretKey";
+
+const generateToken = (user: UserDocument) => {
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    SECRET_KEY,
+    {
+      expiresIn: "365d",
+    }
+  );
+  return token;
+};
+
+export const verifyToken = (token: string) => {
+  const decoded = jwt.verify(token, SECRET_KEY);
+  if (decoded) {
+    return decoded;
+  } else {
+    return null;
+  }
+};
+
+export const _getUser = async (id: string) => {
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+  } catch (error) {
+    Log.error(error);
+    throw new Error(error);
+  }
+};
 
 export const createUser = async (input: DocumentDefinition<UserDocument>) => {
   try {
@@ -13,6 +50,52 @@ export const createUser = async (input: DocumentDefinition<UserDocument>) => {
   }
 };
 
-// const findlUser = async () => {
-//   return await User.find();
-// };
+export const findUser = async (input: DocumentDefinition<UserDocument>) => {
+  try {
+    return await User.findOne({ email: input.email }).select("+password");
+  } catch (error) {
+    Log.error(error);
+    throw new Error(error);
+  }
+};
+
+export const loginUser = async (input: DocumentDefinition<UserDocument>) => {
+  try {
+    const user = await findUser(input);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isMatch = await user.comparePassword(input.password);
+
+    if (!isMatch) {
+      throw new Error("Incorrect password");
+    }
+    return generateToken(user);
+  } catch (error) {
+    Log.error(error);
+    throw new Error(error);
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    return await User.find();
+  } catch (error) {
+    Log.error(error);
+    throw new Error(error);
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await _getUser(id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
